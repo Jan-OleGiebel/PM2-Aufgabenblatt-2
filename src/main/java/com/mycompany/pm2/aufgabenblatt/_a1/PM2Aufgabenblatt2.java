47d69b3a3b73af2610e8a7c311220b14f68925cf
jan-ole.giebel <jan-ole.giebel@jo-technik.com>
2025-05-21 21:46:16 +0200

    Updated code.

--- src/main/java/com/mycompany/pm2/aufgabenblatt/_a1/PM2Aufgabenblatt2.java
+++ src/main/java/com/mycompany/pm2/aufgabenblatt/_a1/PM2Aufgabenblatt2.java
@@ -14,6 +14,7 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
+ * Webseiten, die Inspiration geliefert haben:
  * https://www.baeldung.com/java-maps-streams
  * https://www.geeksforgeeks.org/stream-map-java-examples/
  * https://www.geeksforgeeks.org/stream-filter-java-examples/
@@ -21,40 +22,52 @@ import java.util.stream.Stream;
  * https://www.geeksforgeeks.org/intstream-of-in-java/
  * @author infwuy396
  * 
- *  person.getBirthDate().compareTo(LocalDate.ofYearDay(1984, 1)) == 1 
+ *
  */
+
 public class PM2Aufgabenblatt2 {
 
     public static void main(String[] args) {
-        PM2Map<Integer, Person> persons = new PM2Map<>();
+        PM2Map<String, Person> persons = new PM2Map<>();
         String[] firstNames = {"Tom", "Ole", "Emil", "Moritz", "Sebastian", "Judith", "Julia", "Susanne", "Martina", "Sybille"};
         String[] lastNames = {"Müller", "Mühlhausen", "Spree", "Klein", "Neuhaus", "Meier", "Schulz", "Heinze", "Groß", "Hoffmann"};
         
         Random randomGenerator = new Random();
         
         for(var i=0; i<10; i++) {
+            var firstName = firstNames[i];
+            var lastName = lastNames[i];
+            
             var birthDate = LocalDate.ofYearDay(1950, 1);
             
             long yearsToAdd = (long) (randomGenerator.nextFloat()*53+1);
-            long monthsToAdd = (long) (randomGenerator.nextFloat()*11+1);
-            long daysToAdd = (long) (randomGenerator.nextFloat()*30+1);
+            long monthsToAdd = (long) (randomGenerator.nextFloat()*10+1);
+            long daysToAdd = (long) (randomGenerator.nextFloat()*29+1);
         
             birthDate = birthDate.plusYears(yearsToAdd);
             birthDate = birthDate.plusMonths(monthsToAdd);
             birthDate = birthDate.plusDays(daysToAdd);
             
             int numberOfChildren = (int) (randomGenerator.nextFloat()*5);
-            var person = new Person(firstNames[i], lastNames[i], birthDate, numberOfChildren);
-            persons.put(i, person);
+            var person = new Person(firstName, lastName, birthDate, numberOfChildren);
+            persons.put(lastName, person);
         }
         
-        //Stream<ArrayList<Person>> personStream = persons.entrySet().stream().filter(element -> ((Person)((Map.Entry)element).getValue()).getBirthDate().compareTo(LocalDate.ofYearDay(1984, 1)) == 1 );
+        System.out.println("Personen vor dem Filern und Sortieren:");
+        for(Object person: persons.values().toArray()) {
+            System.out.println(((Person)person).toString());
+        }
         
-        //Object[] processedPersonArray = personStream.sorted(Comparator.comparing(Person::getNumberOfChildren))
-                //.toArray();
+        System.out.println("\n");
         
-        //for(Object person: processedPersonArray) {
-        //    System.out.println(((Person)person).toString());
-        //}
+        Object[] processedPersonArray = persons.values().stream()
+                .filter(person -> person.getBirthDate().compareTo(LocalDate.ofYearDay(1984, 1)) < 0)
+                .sorted(Comparator.comparing(Person::getNumberOfChildren).reversed())
+                .toArray();
+        
+        System.out.println("Personen nach dem Filern und Sortieren:");
+        for(Object person: processedPersonArray) {
+            System.out.println(((Person)person).toString());
+        }
     }
 }
